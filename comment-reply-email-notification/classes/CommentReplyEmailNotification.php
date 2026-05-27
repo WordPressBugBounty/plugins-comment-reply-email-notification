e61893e3ac1750ea9009729c7179519b8a89f064
Sean Murphy <sean@iamseanmurphy.com>
2026-05-27 17:19:58 +0000

    1.40.0

--- comment-reply-email-notification/classes/CommentReplyEmailNotification.php
+++ comment-reply-email-notification/classes/CommentReplyEmailNotification.php
@@ -3,7 +3,7 @@ namespace CommentReplyEmailNotification;
 
 class CommentReplyEmailNotification
 {
-    const CREN_VERSION = '1.39.0';
+    const CREN_VERSION = '1.40.0';
 
     /**
      * Constructor
